54351af41fc1fc52e5e702c78663c99233fd482f
SerhiiShapovalov <ShapovalovSV83@gmail.com>
2023-06-20 16:42:41 +0300

    Finished Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
 import Profile from './Profile/Profile';
 import user from '../data/user.json';
+import Statistics from './Statistics/Statistics';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
     <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      <div>
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
+      </div>
     </div>
   );
 };
